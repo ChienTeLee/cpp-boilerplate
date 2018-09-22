e95b83c7187f2de93120a05e388bd1d31537318b
Chien-Te Lee <Dexter.ChienTeLee@gmail.com>
2018-09-22 02:48:55 -0700

    PID controller First Commit

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
-#include <lib.hpp>
 
-int main()
-{
-    dummy();
-    return 0;
+// sample pid program
+
+#include <iostream>
+
+class pidController {
+public:
+	double findVelocity(double, double);
+private:
+	double Kp, Ki, Kd;
+};
+
+double pidController::findVelocity(double desiredVelocity, double currentVelocity) {
+	return 0.5;
+};
+
+
+int main (){
+	pidController controller1;
+	std::cout << controller1.findVelocity(0.0, 0.0) << std::endl;
+	return 0;
 }
+
